971763899607729e9c49a88544dba392fc90e5b9
Orlando <o.posada23@outlook.com>
2025-01-14 23:05:24 -0600

    Ciclos

---
+++ Ciclos/Ejercicio1.java
@@ -0,0 +1,19 @@
+package Ciclos; 
+import javax.swing.JOptionPane;
+public class Ejercicio1{
+    /*
+        Leer un número y mostrar su cuadrado, repetir el proceso hasta que se introduzca un número negativo. 
+     */
+    public static void main(String[] args) {
+        int numero, cuadrado, resultado; 
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero: "));
+        cuadrado = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero al que se va a elevar: "));
+
+        while (numero >= 0){ //Mientras el numero sea 0 o positivo
+            resultado = (int)Math.pow(numero, cuadrado);
+            JOptionPane.showMessageDialog(null, "El numero "+numero+" elevado al cuadrado es: "+resultado);
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero: "));
+            cuadrado = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero al que se va a elevar: "));
+        }
+    }
+}
